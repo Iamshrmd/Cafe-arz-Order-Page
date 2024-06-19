d8df32fbe3ef1e76e607a20938b4479c297bfa36
shrmyrahmd <shrmyrahmd@gmail.com>
2024-06-19 15:37:56 +0430

    add visible variable to questions array

--- src/components/Questions.vue
+++ src/components/Questions.vue
@@ -1,13 +1,10 @@
 <template>
-  <div class="vazirmatn px-3 bg-[#1D213F] text-[#fff]">
-    <span class="font-bold text-xl"> سوالات متداول</span>
+  <div class="vazirmatn px-3">
+    <h5>سوالات متداول</h5>
     <div>
-        <div @click="questionClicked(i)" class="my-2 p-2 border-solid border border-[#fff] rounded hover:cursor-pointer" v-for="(question,i) in questions" :key="i">
-            <div class="flex items-center justify-between">
-                <h4 class="my-2 duration-400" :class="{'text-[#FEBC00]' : question.visible}">{{ question.title }}</h4>
-                <img @click.self="toggleQuestionClicked(i)" class="w-5" src="../assets/imgs/icons8-arrow-50.png" :class="{'transform rotate-180' : question.visible}">
-            </div>
-            <p class="duration-400" :class="{'opacity-100' : question.visible}" v-if="question.visible">{{ question.des }}</p>
+        <div @click="questionClicked(i)" class="my-2 p-2 border-solid border-2 border-[#000]" v-for="(question,i) in questions" :key="i">
+            <h4>{{ question.title }}</h4>
+            <p class="hidden">{{ question.des }}</p>
         </div>
     </div>
   </div>
@@ -17,11 +14,8 @@
 export default {
     props:['questions'],
     methods: {
-        questionClicked(i){
+        questionClicked(){
             this.$emit('questionClicked',i)
-        },
-        toggleQuestionClicked(i){
-            this.$emit('togglequestionClicked',i)
         }
     }
 }
